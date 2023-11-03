3f40cbb8a673958284eb47cb941aa5288e1fba2a
Junior Takaki <you@example.com>
2023-11-03 19:07:55 -0300

    working in Features details

--- script.js
+++ script.js
@@ -9,17 +9,18 @@ function scrollToElement(elementSelector, instance = 0) {
 }
 
 const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
+//const link2 = document.getElementById("link2"); função foi 
 const link3 = document.getElementById("link3");
 
 link1.addEventListener('click', () => {
     scrollToElement('.header');
 });
 
-link2.addEventListener('click', () => {
+// The button Knowledge was been off.
+//link2.addEventListener('click', () => {
     // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
-});
+//   scrollToElement('.header', 1);
+//});
 
 link3.addEventListener('click', () => {
     scrollToElement('.column');
